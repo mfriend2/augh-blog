67f63e09389ba0d2407ba6a086fdede43c34dd9d
Michael Friend <michael.l.friend2@gmail.com>
2022-09-21 16:31:45 -0500

    first commit

---
+++ src/main/java/com/codeup/aughblog/AughBlogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.aughblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AughBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AughBlogApplication.class, args);
+    }
+
+}
