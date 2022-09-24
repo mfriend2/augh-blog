876e8d8d1e697465bfdf8ff1243db1d61a6ce7fc
Michael Friend <michael.l.friend2@gmail.com>
2022-09-23 19:22:58 -0500

    updated

--- src/main/java/com/codeup/aughblog/controllers/UserController.java
+++ src/main/java/com/codeup/aughblog/controllers/UserController.java
@@ -1,4 +1,24 @@
 package com.codeup.aughblog.controllers;
 
+import com.codeup.aughblog.repositories.CommentRepository;
+import com.codeup.aughblog.repositories.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    private UserRepository usersDao;
+    private CommentRepository commentsDao;
+
+    public UserController(UserRepository usersDao, CommentRepository commentsDao) {
+        this.usersDao = usersDao;
+        this.commentsDao = commentsDao;
+    }
+
+    @GetMapping("/register")
+    public String showRegister(Model model) {
+        return "user/register";
+    }
 }
